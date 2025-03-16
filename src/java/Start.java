a0bed059e03bb13b85b82c562d962c422636c0c4
LLlKBAPKA <109931198+LLlKBAPKA@users.noreply.github.com>
2025-03-17 00:41:17 +0300

    а
    а

---
+++ src/java/Start.java
@@ -0,0 +1,25 @@
+/*import org.excellent.common.launch.Launcher;
+
+public class Start {
+
+    public static void main(String[] args) {
+        Launcher.start();
+    }
+}*/
+
+import net.minecraft.client.main.Main;
+
+import java.util.Arrays;
+
+public class Start {
+    public static void main(String[] args) {
+        String assets = System.getenv().containsKey("assetDirectory") ? System.getenv("assetDirectory") : "assets";
+        Main.main(concat(new String[]{"--version", "mcp", "--accessToken", "0", "--assetsDir", assets, "--assetIndex", "1.16", "--userProperties", "{}"}, args));
+    }
+
+    public static <T> T[] concat(T[] first, T[] second) {
+        T[] result = Arrays.copyOf(first, first.length + second.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
+}
